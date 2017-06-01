d72b04821db02c89bf99d6b566bb94037b97de1f
Cynthia Vint <cynthia.vint@istresearch.com>
2017-06-01 15:48:38 -0400

    fixed plotting so 0 val scales to area of 0

--- R/plot_pyramid.r
+++ R/plot_pyramid.r
@@ -22,10 +22,18 @@ plot_pyramid <- function(country.frame,plot.title='Population Pyramid') {
   
   else {
     
+    country.frame <- within(country.frame, 
+                       Age <- factor(Age, 
+                          levels=c('0-4','5-9','10-14','15-19','20-24','25-29',
+                                   '30-34','35-39','40-44','45-49','50-54','55-59',
+                                   '60-64','65-69','70-74','75-79','80-84','85-89',
+                                   '90-94','95-99','100+')))
+    
     p <- ggplot(country.frame, aes(x = Age, y = Population, fill = Gender)) + 
       geom_bar(subset = .(Gender == "Male"), stat = "identity") + 
       geom_bar(subset = .(Gender == "Female"), stat = "identity") + 
       coord_flip() + 
+      scale_size_area() +
       scale_fill_brewer(palette = "Set1") + 
       theme_bw() +
       ggtitle(plot.title)
