a7ef4c55ce9cc718ba4fdc976a7cbfa85aabbdc1
Cynthia Vint <cvint13@live.com>
2017-06-01 14:02:20 -0400

    added r files

---
+++ R/get_data.r
@@ -0,0 +1,38 @@
+library(XML)
+library(reshape2)
+library(plyr)
+library(httr)
+
+#' A Data Fetching Function for Population Pyramids
+#'
+#' This function pulls down census population data and puts it into a nice dataframe.
+#' @param country Enter ISO country code (e.g. 'IZ' for Iraq)
+#' @param year Enter 4-digit year (e.g. 2012)
+#' @keywords census population dataframe age gender
+#' @export
+#' @examples
+#' get_data('IZ', 2012)
+
+get_data <- function(country, year) {
+  c1 <- "http://www.census.gov/population/international/data/idb/region.php?N=%20Results%20&T=10&A=separate&RT=0&Y="  
+  c2 <- "&R=-1&C="
+  url <- paste0(c1, year, c2, country)
+  print(url)
+  url <- GET(url)
+  df <- data.frame(readHTMLTable(rawToChar(url$content),stringsAsFactors=F))
+  keep <- c(2, 4, 5)
+  df <- df[,keep]  
+  names(df) <- c("Age", "Male", "Female")
+  cols <- 2:3
+  df[,cols] <- apply(df[,cols], 2, function(x) as.numeric(as.character(gsub(",", "", x))))
+  df <- df[df$Age != 'Total', ]  
+  df$Male <- -1 * df$Male
+  df$Age <- factor(df$Age, levels = df$Age, labels = df$Age)
+  
+  df.melt <- melt(df, 
+                  value.name='Population', 
+                  variable.name = 'Gender', 
+                  id.vars='Age' )
+  
+  return(df.melt)
+}
---
+++ R/plot_pyramid.r
@@ -0,0 +1,36 @@
+library(ggplot2)
+library(RColorBrewer)
+
+#' A plotting Function for Population Pyramids
+#'
+#' This function pulls down census population data and puts it into a nice dataframe.
+#' @param country.frame Enter dataframe generated from get_data()
+#' @param plot.title (optional) title of plot
+#' @keywords census population dataframe age gender
+#' @export
+#' @examples
+#' plot_pyramid(iraq.frame, 'Iraq Population Pyramid')
+
+plot_pyramid <- function(country.frame,plot.title='Population Pyramid') {
+  
+  if (!is.element('Population',colnames(country.frame)) |
+      !is.element('Age',colnames(country.frame)) | 
+      !is.element('Gender',colnames(country.frame)) ) {
+    return('This is not a valid df. Use get_data() to download your data.')
+  }
+  
+  else {
+    
+    p <- ggplot(country.frame, aes(x = Age, y = Population, fill = Gender)) + 
+      geom_bar(subset = .(Gender == "Male"), stat = "identity") + 
+      geom_bar(subset = .(Gender == "Female"), stat = "identity") + 
+      coord_flip() + 
+      scale_fill_brewer(palette = "Set1") + 
+      theme_bw() +
+      ggtitle(plot.title)
+    
+    plot(p)
+    
+  }
+  
+}
